1ac2e37a9c954670c08c305aede87699fd761aa1
lopesmrafael <rafa.lopes.monte@gmail.com>
2025-11-03 16:18:00 -0300

    corrigindo erros

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 import styles from "./footer.module.css";
 
@@ -7,13 +7,34 @@ function Footer() {
     <footer className={styles.footer}>
       <ul className={styles.social_list}>
         <li>
-          <FaFacebook />
+          <a
+            href="https://github.com/lopesmrafael/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub"
+          >
+            <FaGithub />
+          </a>
         </li>
         <li>
-          <FaInstagram />
+          <a
+            href="https://www.instagram.com/lopesmrafael/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Instagram"
+          >
+            <FaInstagram />
+          </a>
         </li>
         <li>
-          <FaLinkedin />
+          <a
+            href="https://www.linkedin.com/in/rafael-lopes-748b60314"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn"
+          >
+            <FaLinkedin />
+          </a>
         </li>
       </ul>
       <p className={styles.copy_right}>
